95832d82f5d8a6028921fd7434f724d689ee183c
ZakyMalika <zakymalika0987@gmail.com>
2024-03-28 16:49:42 +0700

    menukarkan i dan j ketika selesai step 7, 8 dan 9

--- onlinekesatu/onlinekesatu.cpp
+++ onlinekesatu/onlinekesatu.cpp
@@ -52,5 +52,16 @@ void q_short(int low, int high) {
 			i++; //step 6
 			cmp_count++;
 		}
+		cmp_count++;
+		//search for an element less than or equal to pivot
+		while ((arr[j] > pivot) && (j >= low)) { //step 7
+			j--; //step 8
+			cmp_count++;
+		}
+		cmp_count++;
+		if (i < j) { //step 9
+			// swap the elemnt at index i whit the elemnt at index j
+			swap(i, j);
+		}
 	}
 }
